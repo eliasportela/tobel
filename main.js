bcd1288f4548c3dccade15677bb3d2cf18326573
eliasportela <edwportela@gmail.com>
2022-03-04 14:58:18 -0300

    edwp-md

--- main.js
+++ main.js
@@ -36,9 +36,9 @@ if (isPackaged) {
   //   openAtLogin: true,
   //   path: app.getPath('exe')
   // });
-  //
-  // const { autoUpdater } = require('electron-updater');
-  // autoUpdater.checkForUpdates();
+
+  const { autoUpdater } = require('electron-updater');
+  autoUpdater.checkForUpdates();
 }
 
 app.userAgentFallback = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36';
